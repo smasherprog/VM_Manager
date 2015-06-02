8aed43e597f7459dfa4e760794b711d1f7b75c58
smasherprog <smasherprog@gmail.com>
2015-06-01 22:25:34 -0700

    Missing files

---
+++ VM_Manager/Domain/Domain_Details.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace VM_Manager.Domain
+{
+    partial class Domain_Details
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Domain_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Domain_Details";
+            this.Size = new System.Drawing.Size(428, 485);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
